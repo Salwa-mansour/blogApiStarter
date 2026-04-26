8bb9ce125f33e044d21b1c13ddeec8806ff0a3eb
Salwa-mansour <alqadasisal@gmail.com>
2026-04-26 07:11:07 +0300

    fix nelify functions

---
+++ server/netlify/functions/api.js
@@ -0,0 +1,40 @@
+// Path: app.js
+
+const express = require("express");
+const serverless = require("serverless-http");
+const cookieParser = require("cookie-parser"); // ADDED: To read refresh tokens from cookies
+const cors = require("cors"); // ADDED: To allow your frontend to talk to the API
+const authRouter = require("./routes/authRouter");
+const categoryRouter = require("./routes/categoryRouter")
+const postRouter = require("./routes/postRouter");
+const commentsRouter = require('./routes/commentsRouter');
+
+const app = express();
+
+
+app.use(cors({ origin: `http://localhost:${process.env.urlPORT || 3000}`, credentials: true })); // ADJUSTED: for frontend communication
+app.use(express.json()); // ADJUSTED: To handle JSON API requests instead of form-data only
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); // ADDED: Crucial for HttpOnly cookie security
+
+app.use("/", authRouter);
+app.use("/category",categoryRouter);
+app.use("/posts",postRouter );
+app.use('/posts/:postId/comments',commentsRouter);
+
+ //------------------end of routes--------------
+app.use('/{*splat}', async (req, res) => {
+    // *splat matches any path without the root path. If you need to match the root path as well /, you can use /{*splat}, wrapping the wildcard in braces.
+    //res.sendFile(path.join(__dirname,'views','404.html'))
+      res.status("404").json( { message: `path ${req.originalUrl} not found ` } );
+  });
+ 
+//  const PORT = process.env.PORT || 3000;
+//  app.listen(PORT, (error) => {
+//    if (error) {
+//      throw error;
+//    }
+//    console.log(`Express app listening on port ${PORT}!`);
+//  });
+// production export
+ module.exports =serverless(app);
