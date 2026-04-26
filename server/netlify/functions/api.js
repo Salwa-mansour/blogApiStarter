8440c985fbe3fb1b52af53dfd2494ddb55b3c295
Salwa-mansour <alqadasisal@gmail.com>
2026-04-26 18:22:31 +0300

    fix netlify redircet

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -9,25 +9,27 @@ const categoryRouter = require("../../routes/categoryRouter")
 const postRouter = require("../../routes/postRouter");
 const commentsRouter = require('../../routes/commentsRouter');
 
-const app = express();
+const router = express();
 
 
-app.use(cors({ origin: `http://localhost:${process.env.urlPORT || 3000}`, credentials: true })); // ADJUSTED: for frontend communication
-app.use(express.json()); // ADJUSTED: To handle JSON API requests instead of form-data only
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser()); // ADDED: Crucial for HttpOnly cookie security
+router.use(cors({ origin: `http://localhost:${process.env.urlPORT || 3000}`, credentials: true })); // ADJUSTED: for frontend communication
+router.use(express.json()); // ADJUSTED: To handle JSON API requests instead of form-data only
+router.use(express.urlencoded({ extended: false }));
+router.use(cookieParser()); // ADDED: Crucial for HttpOnly cookie security
 
-app.use("/", authRouter);
-app.use("/category",categoryRouter);
-app.use("/posts",postRouter );
-app.use('/posts/:postId/comments',commentsRouter);
+router.use("/", authRouter);
+router.use("/category",categoryRouter);
+router.use("/posts",postRouter );
+router.use('/posts/:postId/comments',commentsRouter);
 
  //------------------end of routes--------------
 // Standard Express catch-all for 404s
-app.use((req, res) => {
+router.use((req, res) => {
     res.status(404).json({ message: `Path ${req.originalUrl} not found` });
 });
 
+// Tell Express to use this router for the Netlify function path
+app.use("/.netlify/functions/api", router);
 // The CRITICAL export for Netlify
 const handler = serverless(app);
 module.exports.handler = async (event, context) => {
